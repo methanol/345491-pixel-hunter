bb78e763ce87fa3965b41358085cce806c969051
methanol <methanol.b@gmail.com>
2018-07-09 17:40:50 +0700

    задание 8
    TDD - тест работоспособен

---
+++ js/resizer.js
@@ -0,0 +1,24 @@
+function resize(frame, given) {
+  let newSize = {
+    width: given.width,
+    height: given.height
+  };
+
+  if (frame.width < newSize.width) {
+    const ratioW = newSize.width / frame.width;
+
+    newSize.width = frame.width;
+    newSize.height = given.height / ratioW;
+  }
+
+  if (frame.height < newSize.height) {
+    const ratioH = newSize.height / frame.height;
+
+    newSize.width = Math.floor(frame.width / ratioH);
+    newSize.height = frame.height;
+  }
+
+  return newSize;
+}
+
+export {resize as default};
